5827b770e7b2330bae76502f44fb27fa1e4282bd
Paul <dustin.paul.oh@gmail.com>
2022-03-04 13:31:28 -0800

    added modal for login form

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -51,7 +51,7 @@ const sessionReducer = (state = initialState, action) => {
 export const restoreUser = () => async dispatch => {
     const response = await csrfFetch('/api/session');
     const data = await response.json();
-    dispatch(setUser(data.user));
+    dispatch(data.user ? setUser(data.user) : removeUser());
     return response;
 };
 
