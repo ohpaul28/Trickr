d81efd00a81bc63bc289c3a9368447c81be67322
Paul <dustin.paul.oh@gmail.com>
2022-03-03 21:31:39 -0800

    made small changes to index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,9 +5,11 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
+
+//test route for creating xsrf token
+// router.get('/hello/world', function (req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
 
 module.exports = router;
