dc12262dab9a4647bc4e24aefbe4a6f1397536d9
Paul <dustin.paul.oh@gmail.com>
2022-03-03 22:34:20 -0800

    added loginform page

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import * as sessionActions from '../../store/session';
+import {useDispatch, useSelector} from 'react-redux'
+
+function LoginFormPage() {
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+
+    const onSubmit = () => {
+
+
+        dispatch()
+    }
+
+
+    return (
+
+    );
+}
